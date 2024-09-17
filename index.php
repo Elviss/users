ca1ad41d883bb7c1e509ed1ae9cc979027a315cf
elvis <slv_evs@yahoo.com.br>
2024-09-17 11:50:36 -0300

    removed unnecessary code

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<?php
-require_once 'vendor/autoload.php';
-
-use Symfony\Component\Dotenv\Dotenv;
-
-$dotenv = new Dotenv();
-$dotenv->load(__DIR__.'/.env');
-
-require_once 'Database.php';
-
-$database = new Database();
-
-$database->connect($_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
-
-$users = json_encode($database->fetchAll());
-?>
-
 <html>
     <head>
         <title>Users</title>
