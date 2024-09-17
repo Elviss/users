be5192c68fa6f307e8c41e4b407b29bddb4bfc21
elvis <slv_evs@yahoo.com.br>
2024-09-17 02:09:21 -0300

    new create file to execute the creation of new record

---
+++ create.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'vendor/autoload.php';
+
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/.env');
+
+require_once 'Database.php';
+
+$database = new Database();
+
+$database->connect($_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    $result = $database->create($name, $email);
+    echo $result;
+}
