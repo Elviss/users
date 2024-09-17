344a5d1bf60e3e13cb20a6bf89ecf96c65060cf8
elvis <slv_evs@yahoo.com.br>
2024-09-16 21:18:12 -0300

    connected to the database using the Database class

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+require_once 'vendor/autoload.php';
+
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/.env');
+
+require_once 'Database.php';
+
+$database = new Database();
+
+$database->connect($_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+
+
