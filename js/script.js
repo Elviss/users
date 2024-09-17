31ecbc6c64b421142e2d6de0e973d3ac33feabf7
elvis <slv_evs@yahoo.com.br>
2024-09-17 10:50:00 -0300

    new javascript function to delete a user

--- js/script.js
+++ js/script.js
@@ -8,7 +8,29 @@ function addUser() {
         },
         success: function(res) {
             res = JSON.parse(res);
-            console.log(res);
+
+            Toastify({
+                text: res.msg,
+                duration: 3000,
+                close: true,
+                style: {
+                    background:  res.status === '200' ? 'green' : 'red',
+                }
+            }).showToast();
+        },
+    })
+}
+
+function deleteUser(id) {
+    $.ajax({
+        type: "POST",
+        url: 'delete.php',
+        data: {
+            id: id
+        },
+        success: function(res) {
+            res = JSON.parse(res);
+
             Toastify({
                 text: res.msg,
                 duration: 3000,
